0dc73a16b398e52edc6f65fbe8ef62a559bb14cb
lonebono <vaughnsy15@gmail.com>
2025-08-10 14:11:52 -0700

    updated terminal display

--- main/utils/TerminalDisplay.java
+++ main/utils/TerminalDisplay.java
@@ -34,4 +34,23 @@ public class TerminalDisplay {
         System.out.println("\nUser " + sender + " is sending you a file: " + filename + ". Do you accept?");
         System.out.println("----------------");
     }
+
+    public static void displayGroupCreate(String groupName) {
+        System.out.println("\n=== GROUP CREATE ===");
+        System.out.println("You’ve been added to " + groupName);
+        System.out.println("----------------");
+    }
+
+    public static void displayGroupUpdate(String groupName) {
+        System.out.println("\n=== GROUP UPDATE ===");
+        System.out.println("The group \"" + groupName + "\" member list was updated.");
+        System.out.println("----------------");
+    }
+
+    public static void displayGroupMessage(String from, String content) {
+        System.out.println("\n=== GROUP MESSAGE ===");
+        System.out.println(from + " sent \"" + content + "\"");
+        System.out.println("----------------");
+    }
+
 }
